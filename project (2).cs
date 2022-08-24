f8988bc2dbd7293308cacda4eae56adaa5dd3259
akshaysajeevan01 <111997941+akshaysajeevan01@users.noreply.github.com>
2022-08-24 20:31:29 +0530

    Add files via upload

---
+++ project (2).cs
@@ -0,0 +1,13 @@
+﻿using System; 
+ 
+namespace HelloWorld 
+{ 
+  class Program 
+  { 
+    static void Main(string[] args) 
+    { 
+      Console.WriteLine("Hello World!");  
+      Console.WriteLine("The current date and Time is "+ DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss")); 
+    } 
+  } 
+}
